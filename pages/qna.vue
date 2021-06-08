c30a425e1c15ad26dee95267c8233718d1e501bd
joannashin <joannashin3@gmail.com>
2021-06-09 00:15:58 +0900

    qna 이메일보내기 js 적용

--- pages/qna.vue
+++ pages/qna.vue
@@ -13,12 +13,74 @@
             </header>
         </section>
 
-    </section>
+        <!-- <form @submit="sendEmail()"> -->
+            <label>
+                <input type="text" placeholder="이메일을 입력하세요." maxlength="30" v-model="content.emailid">
+            </label> 
+            <label>
+                <input type="text" placeholder="이름을 입력하세요." maxlength="30" v-model="content.sendname">
+            </label> 
+            <label>
+                <input type="text" placeholder="제목을 입력하세요." maxlength="30" v-model="content.title">
+            </label> 
+            <label>
+                <textarea type="text" placeholder="내용을 입력하세요." maxlength="100" v-model="content.message"></textarea>
+            </label> 
+            <button type="button" @click="sendEmail($event)">보내기</button>
+        <!-- </form> -->
+</section>
 </template>
 <script>
 
-export default {
+import emailjs from "emailjs-com";
 
+export default {
+    data() {
+        return {
+            SERVICE_ID: "service_ewx0f38",
+            TEMPLATE_ID: "template_th1s929",
+            content: {
+                emailid: "",
+                sendname: "",
+                title: "",
+                message: ""
+            }
+        }
+    },
+
+    mounted() {
+        // this.sendEmail();
+    },
+
+    methods: {
+        sendEmail(e) {
+            // console.log("test",emailjs,this.SERVICE_ID)
+
+            const templateParams = {
+                // emailid: "emailid",
+                // sendname: "sendname",
+                // title: "title",
+                // message: "messagemessagemessage"
+                email: this.content.emailid,
+                name: this.content.sendname,
+                title: this.content.title,
+                message: this.content.message,
+            };
+
+            emailjs
+                .send("service_ewx0f38", "template_th1s929", templateParams, "user_rd9DdB4KQe2PyiiUTxon5")
+                .then(
+                    function(response) {
+                        console.log("success", response)
+                    },
+
+                    function (error) {
+                        console.log("Error")
+                    }
+                )
+            
+        }
+    }
 }
 </script>
 
