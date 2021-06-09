d0fa41e3951a38d68497eb9832a7132af196f7cb
jieunshin <joannashin3@forbiz.co.kr>
2021-06-09 19:44:17 +0900

    필수값 체크, 항목값 변경

--- pages/qna.vue
+++ pages/qna.vue
@@ -3,11 +3,11 @@
         <h2 class="fb__page__title">문의/제안</h2>
 
         <div class="fb__qna__form">
-            <span class="fb__qna__tip">이메일로 문의 / 제안을 보내주세요.</span>
+            <!-- <span class="fb__qna__tip">이메일로 문의 / 제안을 보내주세요 !</span> -->
 
             <label>
-                <span class="fb__qna__label">연락 받으실 이메일</span>
-                <input type="text" placeholder="연락을 받으실 이메일을 입력하세요." maxlength="30" v-model="content.email">
+                <span class="fb__qna__label">연락 받으실 이메일 *</span>
+                <input type="text" placeholder="연락을 받으실 이메일을 입력하세요." maxlength="30" v-model="content.reply_to">
             </label> 
 
             <label>
@@ -16,11 +16,12 @@
             </label> 
 
             <label>
-                <span class="fb__qna__label">제목</span>
+                <span class="fb__qna__label">제목 *</span>
                 <input type="text" placeholder="제목을 입력하세요." maxlength="30" v-model="content.title">
             </label> 
 
             <label>
+                <span class="fb__qna__label">내용 *</span>
                 <textarea type="text" placeholder="내용을 입력하세요." maxlength="100" v-model="content.message"></textarea>
             </label> 
 
@@ -32,6 +33,7 @@
 </template>
 <script>
 
+//https://www.emailjs.com/
 import emailjs from "emailjs-com";
 
 export default {
@@ -41,7 +43,7 @@ export default {
             TEMPLATE_ID: "template_th1s929",
             API_KEY: "user_rd9DdB4KQe2PyiiUTxon5",
             content: {
-                email: "",
+                reply_to: "",
                 name: "",
                 title: "",
                 message: ""
@@ -51,25 +53,36 @@ export default {
 
     methods: {
         sendEmail() {
-            const templateParams = {
-                email: this.content.email,
+            if ( !this.content.reply_to 
+                 || !this.content.title 
+                 || !this.content.message 
+            ) {
+                alert("필수 입력란을 확인해주세요.");
+                return ;
+            }
+
+            this.requestEmailJs();
+        },
+
+        requestEmailJs() { 
+             const templateParams = {
+                reply_to: this.content.reply_to,
                 name: this.content.name,
                 title: this.content.title,
                 message: this.content.message,
             };
 
             emailjs
-                // .send("service_ewx0f38", "template_th1s929", templateParams, "user_rd9DdB4KQe2PyiiUTxon5")
                 .send (this.SERVICE_ID, this.TEMPLATE_ID, templateParams, this.API_KEY)
                 .then (
                     function(response) {
                         console.log("success", response)
-                        alert("이메일을 보냈습니다! <br> 곧 연락드리겠습니다. 감사합니다!");
+                        alert("이메일을 보냈습니다! \n 곧 연락드리겠습니다. 감사합니다!");
                     },
 
                     function (error) {
                         console.error("Error", error)
-                        alert("오류가 발생하였습니다.<br> 잠시 후 다시 시도 부탁드립니다.");
+                        alert("오류가 발생하였습니다.\n 잠시 후 다시 시도 부탁드립니다.");
                     }
                 )
         }
